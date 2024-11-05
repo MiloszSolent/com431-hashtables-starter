4c709fbcde94703ac322ad9b3960a832ef26fc3c
MiloszSolent <0jasim37@solent.ac.uk>
2024-11-05 16:00:43 +0000

    Did it

---
+++ Hashtable.py
@@ -0,0 +1,42 @@
+from operator import index, indexOf
+
+from linkedlist import TuplesLinkedList
+
+class HashTable:
+    def __init__(self, nbuckets):
+        #list comprehension
+        #Creates a list of "nbuckets" empty linked lists
+        #each position (bucket) will contain an empty linked list
+        self.internalList = [TuplesLinkedList() for i in range(nbuckets)]
+
+    def calcHashCode(self, key):
+        #Calculate the hash code of the key
+        #The sum of the ascii codes of each character in the key
+        #ord() - gets the ascii code of a character
+
+        hashcode = 0
+
+        for i in range(0, len(key)):
+            hashcode += ord(key[i])
+
+        hashcode %= len(self.internalList)
+
+        return hashcode
+
+    #add the key/value pair to the linked list in the appropriate bucket
+    def put(self, key, value):
+        hashcode = self.calcHashCode(key)
+
+        TuplesLinkedList.add(self.internalList[hashcode], key, value)
+
+    #retrieve the value associated with the given key
+    def get(self, key):
+        return self.internalList[self.calcHashCode(key)].find(key)
+
+internalList = HashTable(4)
+internalList.put("cat", 32)
+internalList.put("dog", 33)
+internalList.put("god", 33)
+
+print(internalList.get("dog"))
+print(internalList.get("god"))
--- linkedlist.py
+++ linkedlist.py
@@ -6,10 +6,13 @@ class TuplesLinkedList:
         self.last = None
 
     # TODO modify "add()" so that it takes a key and a value as parameters and 
-    # creates a  tuple using them. The Node should then be created using the 
+    # creates a tuple using them. The Node should then be created using the
     # tuple.
-    def add(self, value):
-        n = Node(value)
+    def add(self, key, value):
+        n = Node((key, value))
+        #n.value is now a tuple of key and value = n.value = ("cat", 32)
+        # rather than just a single value as key and value are being passed through as a tuple
+
         if self.first is None:
             self.first = n
             self.last = n
@@ -19,6 +22,7 @@ class TuplesLinkedList:
 
     def get(self, index):
         counter = 0
+
         currentNode = self.first
         while currentNode is not None:
             if counter == index:
@@ -31,11 +35,17 @@ class TuplesLinkedList:
     # TODO modify "find()" so that it takes a KEY as a parameter  and searches 
     # the linked list until it finds a tuple with that key. It should then
     # return the value (i.e. the second member of the tuple)
-    def find(self, searchInput):
+    def find(self, key):
         currentNode = self.first
+
         while currentNode is not None:
-            if currentNode.value == searchInput:
+            if currentNode.value[0] == key:
                 return currentNode
             else:
                 currentNode = currentNode.next
         return None
+
+LinkedList = TuplesLinkedList()
+#TuplesLinkedList.add(LinkedList, "cat", 32)
+#TuplesLinkedList.add(LinkedList, "dog", 33)
+#print(TuplesLinkedList.find(LinkedList, "cat"))
